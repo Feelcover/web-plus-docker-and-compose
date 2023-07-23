a98775708788824487eeda117635b97ffd3aa2c9
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-07-24 01:50:44 +0300

    rm

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-import helmet from 'helmet';
 import { nestCsrf } from 'ncsrf';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
@@ -13,7 +12,6 @@ async function bootstrap() {
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.use(cookieParser());
   app.use(nestCsrf());
-  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
